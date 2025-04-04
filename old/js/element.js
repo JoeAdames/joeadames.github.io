0f6f22f0939ea5de79eb5e97cf57a09160827d33
JDA <jadames14@gmail.com>
2025-04-04 13:01:40 -0400

    init vue

--- old/js/element.js
+++
@@ -1,9 +0,0 @@
-class ElementFactory {
-    static create(properties) {
-        const elem = document.createElement(properties.element);
-        for (let i in properties) {
-            elem[i] = properties[i];
-        }
-        return elem;
-    }
-}
