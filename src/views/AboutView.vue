4644265221a9876825817eeaf2581931462e328e
JDA <jadames14@gmail.com>
2025-04-09 12:28:46 -0400

    Visual content review, Remove vue stock code.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,48 +1,5 @@
+<script lang="ts"></script>
 <template>
-  <div class="about">
-    <h3>About Me</h3>
-    <p>
-      I’ve always been a problem solver—first with computers, then with customers, and now through
-      code.
-    </p>
-    <p>
-      My journey into tech didn’t start in a classroom. It started when I was 12, building my first
-      PC with help from a friend. What began as a way to play games turned into something bigger: I
-      was theming Myspace profiles, exploring open-source tools like Foobar2000, and accidentally
-      wiping my hard drive while trying to fix it through the command line. That moment—panic,
-      recovery, learning—was my first real lesson in how tech works _under_ the surface. I didn’t
-      know it then, but I was already on my way.
-    </p>
-    <p>
-      My early career was in retail. I sold components and electronics at RadioShack, then moved to
-      Verizon, where I found I was just as strong at solving technical issues as I was at making
-      sales. I eventually focused full-time on client solutions. At the same time, I was in college
-      studying automotive technology, chasing what I thought was my path—until I realized fixing
-      cars felt more like a chore, while working with computers felt like home.
-    </p>
-    <p>
-      A turning point came when I was offered a role at a civil engineering firm. It wasn’t my
-      field, but I loved the culture and the collaborative environment. Around then, a friend
-      introduced me to a coding bootcamp at Rutgers. That’s when everything clicked. Web development
-      was the thing I didn’t know I’d been preparing for all along.
-    </p>
-    <p>
-      Since then, I’ve worked across multiple teams and companies, constantly leveling up and
-      expanding my skill set. What’s kept me grounded throughout is my love for figuring things
-      out—whether it’s debugging code, customizing a UI, or helping a teammate understand something
-      clearly. Communication has always been my edge, and tech became the space where I could
-      combine that with real, creative problem-solving.
-    </p>
-    <p>I'm still learning. Still building. Still curious. That hasn’t changed.</p>
-  </div>
+  <div>About</div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
